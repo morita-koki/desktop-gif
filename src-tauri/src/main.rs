6f52d9e74bca7457c23799146b85d170a4163f6c
morita-koki <morita82007koki@gmail.com>
2023-12-31 00:45:38 +0900

    click時にbackend処理を呼び出せるよう更新

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,21 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn open_config_window() -> String {
+    format!("open_config_window() called!")
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![ 
+            open_config_window
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 // import { appWindow }  from "@tauri-apps/api/window"
 import confuseDog from "./assets/confuse.gif"
 
+const click_handler = () => {
+  invoke("open_config_window").then(msg => console.log(msg));
+  console.log("click_handler() called");
+}
+
 function App() {
   return (
-    <div data-tauri-drag-region className="container">
+    <div onClick={click_handler} data-tauri-drag-region className="container">
       <img data-tauri-drag-region src={confuseDog} alt="confused dog gif" width="100vw" height="100vh"/>
     </div>
   );
